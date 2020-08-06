ed0f4cd17aa96b5be7c831278e04cbde1040b129
Christopher Paladino <christopher.nascimento@engdb.com.br>
2020-08-06 17:35:11 -0300

    added try/catch at inserction database

--- classes/server/src/routes.ts
+++ classes/server/src/routes.ts
@@ -9,7 +9,6 @@ interface ScheduleItem {
     to:string;
 };
 
-
 routes.post('/classes', async (request, response)=>{
     const {
         name,
@@ -21,35 +20,51 @@ routes.post('/classes', async (request, response)=>{
         schedule,
     } = request.body;
 
-    const insertedUsersIds = await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    });
+    //criando uma transaction
+    const trx = await db.transaction();
 
-    const user_id = insertedUsersIds[0];
+    try{
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+        });
+    
+        const user_id = insertedUsersIds[0];
+    
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+        });
+    
+        const class_id = insertedClassesIds[0];
+    
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) =>{
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to),
+            };
+        });
+    
+        await trx('class_schedule').insert(classSchedule);
+    
+        await trx.commit();
 
-    const insertedClassesIds = await db('classes').insert({
-        subject,
-        cost,
-        user_id,
-    });
+        return response.status(201).send();
+    }catch(err){
 
-    const class_id = insertedClassesIds[0];
+        await trx.rollback();
+        
+        return response.status(400).json({
+            error: 'Unexpected error while creating new class'
+        });
+    }
+    
 
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) =>{
-        return {
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHourToMinutes(scheduleItem.from),
-            to: convertHourToMinutes(scheduleItem.to),
-        };
-    });
-
-    await db('class_schedule').insert(classSchedule);
-
-    return response.send();
 });
 
 export default routes;
