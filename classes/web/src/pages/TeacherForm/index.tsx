dd0ad0558da542bc63fbed9c86ed0d48f945dd0c
Christopher Paladino <christopher.nascimento@engdb.com.br>
2020-08-09 12:44:41 -0300

    created textarea, select components

--- classes/web/src/pages/TeacherForm/index.tsx
+++ classes/web/src/pages/TeacherForm/index.tsx
@@ -3,6 +3,8 @@ import PageHeader from '../../components/PageHeader';
 import './styles.css';
 import Input from '../../components/Input';
 import warningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 function TeacherForm() {
     return(
@@ -19,12 +21,13 @@ function TeacherForm() {
                     <Input name="name" label="Nome completo" />
                     <Input name="avatar" label="Avatar" />
                     <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biografia" />
 
                 </fieldset>
                 <fieldset>
                     <legend>Sobre a aula                      
                     </legend>
-                    <Input name="subject" label="Matéria" />
+                    <Select name="subject" label="Matéria" />
                     <Input name="cost" label="Custo da sua hora por aula" />
                 </fieldset>
                 <footer>                    
