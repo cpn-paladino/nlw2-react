1c98c3394b3a4f237979c6c006f8ba3cd1b3fd45
Christopher Paladino <christopher.nascimento@engdb.com.br>
2020-08-10 13:39:50 -0300

    added createNewConnection function

--- classes/web/src/components/TeacherItem/index.tsx
+++ classes/web/src/components/TeacherItem/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher{
     id: number,
@@ -17,6 +18,12 @@ interface TeacherItemProps{
 }
 
 const  TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+        api.post('connections',{
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
@@ -32,7 +39,7 @@ const  TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
                     Preço/hora
                     <strong>R$ {teacher.cost}</strong>
                 </p>
-                <a href={  `https://wa.me/${teacher.whatsapp}` } target="_blank">
+                <a href={ `https://wa.me/${teacher.whatsapp}` } target="_blank" onClick={createNewConnection}>
                     <img src={whatsappIcon} alt="Entrar em contato"/>
                     Entrar em contato
                 </a>
