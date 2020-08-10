5d1025ebbf47d5640c51d7d621ed06b64585eaac
Christopher Paladino <christopher.nascimento@engdb.com.br>
2020-08-10 11:29:20 -0300

    created api to show data of teachers on study page

--- classes/web/src/components/TeacherItem/index.tsx
+++ classes/web/src/components/TeacherItem/index.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
 
-function TeacherItem(){
+export interface Teacher{
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: string,        
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const  TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
     return (
         <article className="teacher-item">
             <header>
-                 <img src="https://avatars3.githubusercontent.com/u/22564040?s=460&u=e3b7d24311a454479a5b80a90691bbb33064fa1a&v=4" alt="Paladino"/>
+                 <img src={teacher.avatar} alt={teacher.name} />
                  <div>
-                     <strong>Paladino</strong>
-                     <span>Programação em Python</span>
+                     <strong>{teacher.name}</strong>
+                     <span>{teacher.subject}</span>
                  </div>
             </header>
-            <p>
-            Desenvolvedor Python a vários anos, entusiasta de software livre.
-            <br /><br />
-            Apaixonado por aprender e ter novas ideias de melhoria de processo.
-            </p>
+                <p>{teacher.bio}</p>
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 250,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
                 <button type='button'>
                     <img src={whatsappIcon} alt="Entrar em contato"/>
