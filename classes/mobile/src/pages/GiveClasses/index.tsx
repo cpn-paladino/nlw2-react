7d8641a20131bbd617c2fd705c9786a631437d6d
Christopher Paladino <christopher.nascimento@engdb.com.br>
2020-08-16 19:31:30 -0300

    created give-class page structure

--- classes/mobile/src/pages/GiveClasses/index.tsx
+++ classes/mobile/src/pages/GiveClasses/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import styles from './styles';
+import GiveClassesBgImage from '../../assets/images/give-classes-background.png';
 
 function GiveClasses() {
-    return <View />
+    return (
+    <View style={styles.container}>
+        <ImageBackground resizeMode="contain" source={GiveClassesBgImage} style={styles.content}>
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+            Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+        </Text>    
+        </ImageBackground>
+    </View>
+    );
 }
 
 export default GiveClasses;
--- classes/mobile/src/pages/Landing/index.tsx
+++ classes/mobile/src/pages/Landing/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from "react-native";
 import {useNavigation, NavigationHelpersContext} from '@react-navigation/native'
-
 import styles from './styles';
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from "../../assets/images/icons/study.png";
 import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
+import {RectButton} from 'react-native-gesture-handler'
 
 function Landing() {
     const {navigate} = useNavigation();
@@ -25,15 +25,17 @@ function Landing() {
                 </Text>       
             </Text>
             <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                <RectButton style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>Estudar</Text>
-                </TouchableOpacity>
+                </RectButton>
 
-                <TouchableOpacity onPress={handleNavigateToGiveClassesPage} style={[styles.button, styles.buttonSecondary]}>
+                <RectButton 
+                onPress={handleNavigateToGiveClassesPage}
+                style={[styles.button, styles.buttonSecondary]}>
                     <Image source={giveClassesIcon} />
                     <Text style={styles.buttonText}>Dar aulas</Text>
-                </TouchableOpacity>            
+                </RectButton>            
             </View>
             <Text style={styles.totalConnections}>
                 Total de 285 conexões já realizadas {' '}
