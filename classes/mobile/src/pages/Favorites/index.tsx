02b589e581ba5b2c6a0e47155db1afc95e5d5e47
Christopher Paladino <christopher.nascimento@engdb.com.br>
2020-08-17 23:24:06 -0300

    added studyTabs navigation

---
+++ classes/mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+function Favorites() {
+    return (
+        <View style={styles.container}/>
+    );
+}
+
+export default Favorites;
--- classes/mobile/src/routes/AppStack.tsx
+++ classes/mobile/src/routes/AppStack.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
+import StudyTabs from './StudyTabs';
 
 const {Navigator, Screen} = createStackNavigator(); 
 
@@ -11,7 +12,8 @@ function AppStack() {
     <NavigationContainer>
         <Navigator screenOptions={{ headerShown: false }}>
             <Screen name="Landing" component={Landing}/>
-            <Screen name="GiveClasses" component={GiveClasses}/>            
+            <Screen name="GiveClasses" component={GiveClasses}/>   
+            <Screen name="Study" component={StudyTabs}/>                                    
         </Navigator>
     </NavigationContainer>
  );    
